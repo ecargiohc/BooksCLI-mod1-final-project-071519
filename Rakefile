07037fdc7f92a8108423e164d6c2eedf9c25d4a3
Marisa <you@example.com>
2019-07-31 11:00:20 -0700

    CLI RUN

--- Rakefile
+++ Rakefile
@@ -11,4 +11,10 @@ task :console do
   Pry.start
 end
 
+desc 'See list of books from Google'
+task :Google_books do 
+  x = 1
+  GoogleBooks.search(x)
+end
+
  
--- bin/google_books.rb
+++ bin/google_books.rb
@@ -3,5 +3,9 @@ URL = "https://www.googleapis.com/books/v1/volumes?q="
 class GoogleBooks
     def self.search(query)
         puts query
+        puts query
+        puts query
+        puts query
+        puts query
     end
 end
