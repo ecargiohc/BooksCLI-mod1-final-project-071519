b285b2ad8860cca4a8f792918222553162563a3a
ecargiohc <therealgracechoi@gmail.com>
2019-07-31 11:01:52 -0700

    wrote models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
     has_many :reviews
 
-    #queries here
+#queries here
     def self.title
         self.all.map do |b|
             b.title
@@ -18,6 +18,18 @@ class Book < ActiveRecord::Base
         self.all.map do |b|
             b.genre 
         end
+    end
+#instance of a book review; reviewS of a specific book
+    def book_review
+        Review.all.select do |r|
+            r.book == self
+        end
+    end
+
+#instance of book reviews; total number of book reviews of a specific book
+    def total_reviews
+        Review.all.select {|r|
+            r.book == self}.count
     end
 
     
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,27 @@
 class Review < ActiveRecord::Base
     belongs_to :user
     belongs_to :book
+
+#lists content of a given book
+    def content(book)
+        self.all.map do |r|
+            r.content 
+        end
+    end
+
+    def self.user_id
+        self.all.map do |r|
+            r.user_id
+        end
+    end
+
+    def self.book_id
+        self.all.map do |r|
+            r.book_id
+        end
+    end
+
+
+
+
 end
