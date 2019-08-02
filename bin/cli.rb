4ae28d20bf863bdd35fd1d27de39a5b9979c51c9
ecargiohc <therealgracechoi@gmail.com>
2019-08-02 10:24:42 -0700

    crud + new description feature added

--- bin/cli.rb
+++ bin/cli.rb
@@ -14,34 +14,34 @@ class CLI
         is_looping = true
         while is_looping 
             puts "What would you like to do? Enter a number."
-            puts "1. search for a new book"
-            puts "2. see your reviews"
-            puts "3. search for reviews on a book"
-            puts "4. see a book description" #scrap if no time
-            puts "5. write a review"
-            puts "6. update your review?"
-            puts "7. delete your review?"
-            puts "8. See all books"
-            puts "9. Get me out of here"
-            
+            puts "1. Search for a book"
+            puts "2. Read up on a book's plot"
+            puts "3. Look at others' reviews"
+            puts "4. See your reviews" 
+            puts "5. Write a review!"
+            puts "6. Update your review(s)"
+            puts "7. Delete your review(s)"
+            puts "8. See your saved list of books to review"
+            puts "9. Just get me out of here"
+        
             user_choice = STDIN.gets.chomp
             if user_choice == "1" #DONE
                 self.search
-            elsif user_choice == "2" 
-                self.see_user_reviews(user)
+            elsif user_choice == "2" #DONE
+                self.see_book_description
             elsif user_choice == "3" #DONE
                 self.see_book_reviews
-            elsif user_choice == "4" 
-                self.see_book_description
-            elsif user_choice == "5"
-                self.create_review(user) #CREATE/ADD DONE
-            elsif user_choice == "6"
-                self.update_review(user) #UPDATES DONE
-            elsif user_choice == "7"
-                self.delete_review(user) #DELETES DONE
-            elsif user_choice == "8"
-                self.see_books #DONE
-            elsif user_choice == "9"
+            elsif user_choice == "4" #DONE
+                self.see_user_reviews(user)
+            elsif user_choice == "5" #CREATE/ADD DONE
+                self.create_review(user) 
+            elsif user_choice == "6"  #UPDATES DONE
+                self.update_review(user)
+            elsif user_choice == "7" #DELETES DONE
+                self.delete_review(user) 
+            elsif user_choice == "8" #DONE
+                self.see_books     
+            elsif user_choice == "9" #DONE
                 puts "Ok, bye for now!"
                 is_looping = false
             end
@@ -79,16 +79,16 @@ class CLI
     def self.see_user_reviews(user)
         # binding.pry
         Review.all.filter do |review|
-            if review.user.name == user
-                 puts "#{review.review}"
-            else 
-                puts "You don't have any reviews on this book yet :("
+            if review.user.name == user.name
+                 puts "Your thoughts on '#{review.book.title}': '#{review.review}'"
+            # else 
+            #     puts "You don't have any reviews on this book yet :("
             end
         end
     end
 
     def self.see_book_reviews #DONE
-        puts "Which book's reviews would you like to see?"
+        puts "Which book's reviews would you like to see? Enter title:"
         book_title = STDIN.gets.chomp
         book_reviews = Review.all.filter{|r| r.book.title == book_title}
         if book_reviews.count > 0
@@ -97,26 +97,20 @@ class CLI
             end
         else
             puts "Nobody has reviewed on this book yet"
-        end
+        end 
     end
 
     def self.see_book_description
-        contents = GoogleBooks.search(query)
-        contents.each do |content|
-            puts content["description"]
-        end
-
-        puts "Would you like to see this book's synopsis?"
-        user_choice = STDIN.gets.chomp.downcase
-        if user_choice == 'y'
-            contents[0]
-        else
-            puts ""
+        puts "Enter the synopsis of the book you'd like to see:"
+        user_input = STDIN.gets.chomp
+        results = GoogleBooks.search(user_input)
+        results.each do |result|
+            puts result["description"]
         end
     end
 
     def self.create_review(user)
-        puts "So you have opinions. Which book?"
+        puts "So you have opinions. Which book? Enter title:"
         book_choice = STDIN.gets.chomp
         book = Book.find_by(title: book_choice)
         if book == nil
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,20 @@ User.destroy_all
 Review.destroy_all
 
 infinite = Book.create(title: "Infinite Jest", author: "DFW", genre: "misc")
-lotr = Book.create(title: "LOTR", author: "Tolkein", genre: "fantasy")
+lotr = Book.create(title: "LOTR", author: "Tolkien", genre: "fantasy")
 pretty = Book.create(title:"Me Talk Pretty One Day", author: "Sedaris", genre: "comedy")
 
 tom = User.create(name: "Tom")
 gary = User.create(name: "Gary")
 dave = User.create(name: "Dave")
-randy = User.create(name: "Randy")
+matt = User.create(name: "Matt")
+grace = User.create(name: "Grace")
+marisa = User.create(name: "Marisa")
 
 r1 = Review.create(user: tom, book: infinite, review: "heavy")
-r2 = Review.create(user: tom, book: lotr, review: "cool")
-r3 = Review.create(user: tom, book: pretty, review: "hilarious")
-r4 = Review.create(user: tom, book: lotr, review: "rad")
-r5 = Review.create(user: tom, book: pretty, review: "fun")
-r6 = Review.create(user: tom, book: infinite, review: "crass")
+r2 = Review.create(user: gary, book: lotr, review: "cool")
+r3 = Review.create(user: dave, book: pretty, review: "hilarious")
+r4 = Review.create(user: matt, book: lotr, review: "rad")
+r5 = Review.create(user: grace, book: pretty, review: "fun")
+r6 = Review.create(user: marisa, book: infinite, review: "crass")
 
